51bff9b650466e3dba3e1467397455d3f6ad00ef
yi1306c12 <yi1306c12@gmail.com>
2019-02-20 11:33:30 +0900

    psd -> amp

--- neurofeedback_circle.py
+++ neurofeedback_circle.py
@@ -97,6 +97,6 @@ if __name__ == '__main__':
 
         # display
         band = (8, 12) # alpha band
-        r = psds[(band[0] < freq) & (freq < band[1])].mean()
+        r = psds[(band[0] < freq) & (freq < band[1])].mean()**0.5
         win.circle.setRadius(r)
         win.flip()
